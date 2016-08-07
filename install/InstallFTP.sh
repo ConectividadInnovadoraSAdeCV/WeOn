5979b5328f0ad65fccf0eb13bc36980cfc9ac00c
Paul Barajas <paul.barajas@linux.com>
2016-08-07 14:37:28 -0500

    Adding weon config file to create connection with the ftp server

--- install/InstallFTP.sh
+++ install/InstallFTP.sh
@@ -10,5 +10,7 @@ chmod 755 -R /var/run/weon_daemon
 chmod 755 -R /var/log/weon_daemon
 
 cp config/weon_daemon /etc/init.d/
+cp config/weon.conf /etc/
 chmod u+x /etc/init.d/weon_daemon
+chmod u+x /etc/weon.conf
 sysv-rc-conf weon_daemon on
