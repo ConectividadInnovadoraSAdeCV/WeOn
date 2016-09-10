7411172bd80523adfac51e167dbc7bc27a16e7c3
Paul Barajas <paul.barajas@linux.com>
2016-09-09 22:08:29 -0500

    Update web page interface to improve user experence and avoid the storage of the logging page

--- src/PortalMod/work.php
+++ src/PortalMod/work.php
@@ -1,29 +1,30 @@
 ﻿<?php
-	$ip=$_SERVER['REMOTE_ADDR'];
-	$arp="/usr/sbin/arp ".$ip;
-	$line=shell_exec($arp);
-	$lines=split(" ", $line);
-	$mac=$lines[59];//aqui esta la mac addreess
-	$filename='/home/rock/WeOn/logs/'.date('Y-m-d').'-Connects.txt';
+    $ip=$_SERVER['REMOTE_ADDR'];
+    $arp="/usr/sbin/arp ".$ip;
+    $line=shell_exec($arp);
+    $lines=split(" ", $line);
+    $mac=$lines[59];//aqui esta la mac addreess
+    $filename='/home/rock/WeOn/logs/'.date('Y-m-d').'-Connects.txt';
     $cmd=$mac." | ".$_POST["año"]."-".$_POST["mes"]."-".$_POST["dia"]." | ".$_POST["sexo"]."\n";
 
     $fp = fopen($filename , 'a');
     fwrite($fp, $cmd);
     fclose($fp);
-	//se trata de llamar a el demonio por cuestiones de permisos
-	$manejador=fsockopen("localhost",7000);
-	if(!$manejador)
-	{
-		header("location: index.html");
-	}
-        $resultado="";
-	fputs($manejador,$mac);
-	while(!feof($manejador))
-	{
-		$resultado.=fgetc($manejador);
-	}
-	fputs($manejador, "E");
-    fclose($manejador);
-        echo $resultado;
-	header("Location: http://www.weon.mx/PuntoJaliscoAbierto/descargar.html");
+    $mac_test=shell_exec("iptables -L | grep -iP \"ACCEPT.*".$mac."\"");
+    if (isset($mac_test)) {
+        header("Location: ".$_SERVER['SERVER_NAME'] );
+        exit;  }
+
+    $handler=fsockopen("localhost",7000);
+    $resultado="";
+    fputs($handler,$mac);
+    while(!feof($handler))
+    {
+        $resultado.=fgetc($handler);
+    }
+    fputs($handler, "E");
+    fclose($handler);
+    echo $resultado;
+    header("Location: http://www.weon.mx/PuntoJaliscoAbierto/descargar.html");
+    exit;
 ?>
